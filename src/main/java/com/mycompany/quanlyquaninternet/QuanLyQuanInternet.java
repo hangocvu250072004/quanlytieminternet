976c1a3fc930004ebc89c68fbed2c319030cd06d
Administrator <Administrator@MAY23>
2024-04-07 17:38:55 +0700

    first commit

---
+++ src/main/java/com/mycompany/quanlyquaninternet/QuanLyQuanInternet.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quanlyquaninternet;
+
+/**
+ *
+ * @author Administrator
+ */
+public class QuanLyQuanInternet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        trangchu tc = new trangchu();
+        tc.show();
+    }
+}
